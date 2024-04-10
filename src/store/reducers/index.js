913c374b7471ac259632371b6327187c59664a83
unknown <jeremie2018mihigo@gmail.com>
2024-04-10 06:00:39 +0200

    pages

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -4,10 +4,10 @@ import { combineReducers } from 'redux';
 // project import
 
 import user from '../../Redux/user';
-import Raison from '../../Redux/raison';
+import raison from '../../Redux/raison';
 
 // ==============================|| COMBINE REDUCERS ||============================== //
 
-const reducers = combineReducers({ user, raison : Raison });
+const reducers = combineReducers({ user, raison });
 
 export default reducers;
