fb144db6a775fc146b30db5e8df7b69109a51203
unknown <grace1mihigo@gmail.com>
2024-06-15 12:57:34 +0200

    updaed

--- src/UpdateDemande.jsx
+++ src/UpdateDemande.jsx
@@ -1,212 +1,214 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-import { Language } from "@mui/icons-material";
+import { Input } from 'antd'
+import React, { useEffect } from 'react'
+import { Button, Grid } from '@mui/material'
+import axios from 'axios'
+import { lien } from './Static'
+import DirectionSnackbar from './Control/SnackBar'
+import { Language } from '@mui/icons-material'
+import TextArea from './Control/TextArea'
 import {
-  Box,
-  Button,
   Checkbox,
   FormControl,
+  FormLabel,
   FormControlLabel,
   FormGroup,
-  FormLabel,
-  Grid,
-} from "@mui/material";
-import { Input, message } from "antd";
-import React, { useEffect } from "react";
-import { CreateContexte } from "./Context";
-import TextArea from "./Control/TextArea";
-function UpdateDemande({ demande, close }) {
-  const { socket } = React.useContext(CreateContexte);
-  const [initial, setInitial] = React.useState();
-  const [value, setValue] = React.useState("");
-  const [open, setOpen] = React.useState(true);
-  const [generateLoc, setGenerateLoc] = React.useState(false);
-  const [file, setImage] = React.useState();
-  const [raisonRwrite, setRaisonRwrite] = React.useState("");
+  Box,
+} from '@mui/material'
 
-  const [loadings, setLoadings] = React.useState(false);
+function UpdateDemande({ demande, close }) {
+  const [initial, setInitial] = React.useState()
+  const [value, setValue] = React.useState('')
+  const [message, setMessage] = React.useState('')
+  const [open, setOpen] = React.useState(true)
+  const [generateLoc, setGenerateLoc] = React.useState(false)
+  const [file, setImage] = React.useState()
+  const [raisonRwrite, setRaisonRwrite] = React.useState('')
+
+  const [loadings, setLoadings] = React.useState(false)
 
   const handleChange = (e) => {
-    const { value, name } = e.target;
-    setInitial({ ...initial, [name]: value });
-  };
+    setMessage('')
+    const { value, name } = e.target
+    setInitial({ ...initial, [name]: value })
+  }
 
-  const [location, setLocation] = React.useState(null);
+  const [location, setLocation] = React.useState(null)
 
   function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const altitude = position.coords.altitude;
-    setLocation({ latitude, longitude, altitude });
-    setGenerateLoc(false);
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    const altitude = position.coords.altitude
+    setLocation({ latitude, longitude, altitude })
+    setGenerateLoc(false)
   }
   function error() {
-    console.log("Unable to retrieve your location");
+    console.log('Unable to retrieve your location')
   }
   function handleLocationClick() {
-    setGenerateLoc(true);
+    setGenerateLoc(true)
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
+      navigator.geolocation.getCurrentPosition(success, error)
     } else {
-      setGenerateLoc(false);
+      setMessage('Geolocation not supported')
+      setGenerateLoc(false)
     }
   }
-  const [messageApi, contextHolder] = message.useMessage();
-  const successAlert = (texte, type) => {
-    messageApi.open({
-      type,
-      content: "" + texte,
-      duration: 5,
-    });
-  };
   const sendData = async (e) => {
     try {
-      setLoadings(true);
-      e.preventDefault();
+      setLoadings(true)
+      e.preventDefault()
       if (
-        !initial.reference ||
-        !initial.sat ||
-        !initial.cell ||
-        !initial.sector ||
-        value === "" ||
-        !initial.raison
+        !initial?.reference ||
+        !initial?.sat ||
+        !initial?.cell ||
+        !initial?.sector
       ) {
-        successAlert("Veuillez renseigner les champs", "error");
+        setMessage('Veuillez renseigner les champs')
       } else {
-        let data = {};
-        data.file = file;
-        data.longitude = location?.longitude;
-        data.latitude = location?.latitude;
-        data.altitude = location?.altitude;
-        data.codeAgent = localStorage.getItem("codeAgent");
-        data.codeZone = localStorage.getItem("codeZone");
-        data.codeclient = initial?.codeclient;
-        data.statut = value;
-        data.raison = initial?.raison;
-        data.sector = initial?.sector;
-        data.cell = initial?.cell;
-        data.reference = initial?.reference; //placeholder = Reference
-        data.sat = initial?.sat;
-        data.numero = initial?.numero;
-        data.commune = initial?.commune;
-        data.id = demande._id;
-        data.type = "updated";
-        socket.emit("sendData", data);
+        setMessage('')
+        const datas = new FormData()
+        datas.append('file', file)
+        datas.append('longitude', location?.longitude)
+        datas.append('latitude', location?.latitude)
+        datas.append('altitude', location?.altitude)
+        datas.append('codeAgent', localStorage.getItem('codeAgent'))
+        datas.append('codeZone', localStorage.getItem('codeZone'))
+        datas.append('codeclient', initial?.codeclient)
+        datas.append('statut', value)
+        datas.append('raison', initial?.raison)
+        datas.append('sector', initial?.sector) //placeholder = Sector/constituency
+        datas.append('cell', initial?.cell) //placeholder = Cell/Ward
+        datas.append('reference', initial?.reference) //placeholder = Reference
+        datas.append('sat', initial?.sat)
+        datas.append('numero', initial?.numero)
+        datas.append('commune', initial?.commune)
+        datas.append('id', demande._id)
 
-        setLocation(null);
-        close(false);
-        const form = document.getElementById("formDemande");
-        const fileInput = form.querySelector('input[type="file"]');
-        fileInput.value = "";
-        setImage();
-        successAlert("Done", "success");
+        const response = await axios.put(lien + '/updateDemande', datas)
+
+        if (response.data?._id) {
+          setLocation(null)
+          close(false)
+          // const form = document.getElementById('formDemande')
+          // const fileInput = form.querySelector('input[type="file"]')
+          // fileInput.value = ''
+
+          setMessage('Done : ' + response.data.idDemande)
+        }
+        if (response.status === 201) {
+          setMessage(response.data)
+        }
       }
 
-      setLoadings(false);
+      setLoadings(false)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const returnValue = (champs) => {
     if (
       initial &&
-      initial["" + champs] &&
-      initial["" + champs] !== "undefined"
+      initial['' + champs] &&
+      initial['' + champs] !== 'undefined'
     ) {
-      return initial["" + champs];
+      return initial['' + champs]
     } else {
-      return "";
+      return ''
     }
-  };
+  }
 
   useEffect(() => {
-    setInitial({ ...demande });
+    setInitial({ ...demande })
 
     setLocation({
       latitude: demande.coordonnes.latitude,
       longitude: demande.coordonnes.longitude,
       altitude: demande.coordonnes.altitude,
-    });
+    })
     // donnerStat.filter(x=>x.value === demande.statut)
-    setValue(demande.statut);
-    setRaisonRwrite(demande.raison);
-  }, [demande]);
+    setValue(demande.statut)
+    setRaisonRwrite(demande.raison)
+  }, [demande])
 
   return (
     <>
-      {contextHolder}
       <form id="formDemande">
-        <div style={{ marginBottom: "10px" }}>
+        {message && (
+          <DirectionSnackbar message={message} open={open} setOpen={setOpen} />
+        )}
+        <div style={{ marginBottom: '10px' }}>
           <Input
             placeholder="Code client"
             name="codeclient"
-            value={returnValue("codeclient")}
+            value={returnValue('codeclient')}
             onChange={(e) => handleChange(e)}
           />
         </div>
-        <div style={{ marginBottom: "10px" }}>
+        <div style={{ marginBottom: '10px' }}>
           <Input
             required
             name="commune"
-            value={returnValue("commune")}
+            value={returnValue('commune')}
             onChange={(e) => handleChange(e)}
             placeholder="Commune *"
           />
         </div>
-        <div style={{ marginBottom: "10px" }}>
+        <div style={{ marginBottom: '10px' }}>
           <Input
             required
             name="sector"
-            value={returnValue("sector")}
+            value={returnValue('sector')}
             onChange={(e) => handleChange(e)}
             placeholder="Quartier *"
           />
         </div>
-        <div style={{ marginBottom: "10px" }}>
+        <div style={{ marginBottom: '10px' }}>
           <Input
             name="cell"
-            value={returnValue("cell")}
+            value={returnValue('cell')}
             onChange={(e) => handleChange(e)}
             placeholder="Avenue *"
           />
         </div>
-        <div style={{ marginBottom: "10px" }}>
+        <div style={{ marginBottom: '10px' }}>
           <Input
             name="reference"
-            value={returnValue("reference")}
+            value={returnValue('reference')}
             onChange={(e) => handleChange(e)}
             placeholder="Référence *"
           />
         </div>
-        <div style={{ marginBottom: "10px" }}>
+        <div style={{ marginBottom: '10px' }}>
           <Input
             name="sat"
-            value={returnValue("sat")}
+            value={returnValue('sat')}
             onChange={(e) => handleChange(e)}
             placeholder="Sat *"
           />
         </div>
 
-        <div style={{ marginBottom: "10px" }}>
+        <div style={{ marginBottom: '10px' }}>
           <input
             onChange={(event) => {
-              const file = event.target.files[0];
-              setImage(file);
+              const file = event.target.files[0]
+              setImage(file)
             }}
             type="file"
             accept=".png, .jpg, .jpeg"
           />
         </div>
-
-        <div style={{ marginBottom: "10px" }}>
-          <Box sx={{ display: "flex" }}>
+       
+        <div style={{ marginBottom: '10px' }}>
+          <Box sx={{ display: 'flex' }}>
             <FormControl sx={{ m: 1 }} component="fieldset" variant="standard">
               <FormGroup>
                 <FormControlLabel
-                  onClick={() => setValue("allumer")}
+                  onClick={() => setValue('allumer')}
                   control={
-                    <Checkbox checked={value === "allumer"} name="allumer" />
+                    <Checkbox checked={value === 'allumer'} name="allumer" />
                   }
                   label="Allumé"
                 />
@@ -216,9 +218,9 @@ function UpdateDemande({ demande, close }) {
               <FormLabel component="legend"></FormLabel>
               <FormGroup>
                 <FormControlLabel
-                  onClick={() => setValue("eteint")}
+                  onClick={() => setValue('eteint')}
                   control={
-                    <Checkbox checked={value === "eteint"} name="eteint" />
+                    <Checkbox checked={value === 'eteint'} name="eteint" />
                   }
                   label="Eteint"
                 />
@@ -226,16 +228,16 @@ function UpdateDemande({ demande, close }) {
             </FormControl>
           </Box>
         </div>
-        <div style={{ marginBottom: "10px" }}>
+        <div style={{ marginBottom: '10px' }}>
           <TextArea
             setValue={setRaisonRwrite}
             value={raisonRwrite}
             placeholder="Mentionnez le feedback *"
           />
         </div>
-        <div style={{ marginTop: "10px" }}>
+        <div style={{ marginTop: '10px' }}>
           <Input
-            value={returnValue("numero")}
+            value={returnValue('numero')}
             placeholder="Numero joignable du client"
             name="numero"
             onChange={(e) => handleChange(e)}
@@ -243,16 +245,16 @@ function UpdateDemande({ demande, close }) {
         </div>
         <div
           style={{
-            textAlign: "center",
-            backgroundColor: "#dedede",
-            borderRadius: "20px",
-            padding: "10px",
-            margin: "10px",
+            textAlign: 'center',
+            backgroundColor: '#dedede',
+            borderRadius: '20px',
+            padding: '10px',
+            margin: '10px',
           }}
         >
           <p>
             long : {location?.longitude}
-            {" lat : "}
+            {' lat : '}
             {location?.latitude}
           </p>
         </div>
@@ -266,28 +268,28 @@ function UpdateDemande({ demande, close }) {
               onClick={handleLocationClick}
             >
               <Language fontSize="small" />
-              <span style={{ marginLeft: "10px" }}>
-                {generateLoc && "Patientez..."}
+              <span style={{ marginLeft: '10px' }}>
+                {generateLoc && 'Patientez...'}
               </span>
             </Button>
           </Grid>
           <Grid item xs={6}>
             <Button
-              sx={{ marginLeft: "10px" }}
+              sx={{ marginLeft: '10px' }}
               fullWidth
               className="primary"
               variant="contained"
               onClick={(e) => sendData(e)}
               disabled={loadings}
             >
-              <span style={{ marginLeft: "10px" }}>
-                {loadings ? "Patientez..." : "Modifier"}
+              <span style={{ marginLeft: '10px' }}>
+                {loadings ? 'Patientez...' : 'Modifier'}
               </span>
             </Button>
           </Grid>
         </Grid>
 
-        <div style={{ marginTop: "10px" }}>
+        <div style={{ marginTop: '10px' }}>
           <p>
             Rassurez-vous que vous etes chez le client pour une meilleure
             géolocalisation
@@ -296,7 +298,7 @@ function UpdateDemande({ demande, close }) {
         <div></div>
       </form>
     </>
-  );
+  )
 }
 
-export default UpdateDemande;
+export default UpdateDemande
