cfa62a0131033436fca42edddf17cf63dc9e8a2a
Jérémie Mihigo <98328743+jeremiemihigo@users.noreply.github.com>
2024-08-25 08:36:56 +0200

    Update Static.jsxlin

--- src/Static.jsx
+++ src/Static.jsx
@@ -4,7 +4,8 @@
 // export const lien_image = "http://109.199.122.241:5000/bboxx/image";
 
 // const link = "https://bboxxother.onrender.com";
-const link = "https://visite.bboxxvm.com";
+//const link = "https://visite.bboxxvm.com";
+const link = "https://backendtestserver.onrender.com";
 //const link = "http://localhost:40002";
 
 export const lien = `${link}/bboxx/support`;
